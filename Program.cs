e2a5bc881e7e819145f6c712ff25103ddd4e2f86
Muyiwa Olalekan <muyiwa.olalekan@thebulb.africa>
2020-12-16 13:35:53 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace SecondAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\Users\\MuyiwaOlalekan\\OneDrive - Enov8 Solutions Ltd\\Desktop\\InputOutputFileStream\\new.txt";
+            int countThevalue = 0;
+            DateTime todayDate = DateTime.Now;
+            List <string> textContent = new List<string>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                countThevalue++;
+                Console.WriteLine("Please input the text content {0}",todayDate);
+                string contentTopics = Console.ReadLine();
+                Thread.Sleep(3000);
+                textContent.Add(contentTopics);
+            }
+
+            textContent.Reverse();
+
+            File.AppendAllLines(filePath, textContent);
+
+            Console.ReadLine();
+        }
+    }
+}
